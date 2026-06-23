9995578f97f73e40feaa2e97a7f598e1669c0190
imam syuja <imamsyuja97@gmail.com>
2026-06-23 22:05:25 +0700

    Perbaiki kembali kode agar lulus pengujian

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask  
 app = Flask(__name__)  
 @app.route('/')  
-def hello(): return {'pesan': 'Halo'}  
+def hello(): return {'pesan': 'Hello from CI/CD', 'nim': '230705039'}  
+@app.route('/health')  
+def health(): return {'status': 'ok'}  
 if __name__ == '__main__': app.run(host='0.0.0.0', port=5000) 
