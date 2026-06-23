a5bdb96868aa470758af2c4db4a907feb865b3c0
imam syuja <imamsyuja97@gmail.com>
2026-06-23 21:54:58 +0700

    Sengaja mengubah tulisan menjadi Halo untuk break test

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask  
-app = Flask(__name__)  
-@app.route('/')  
-def hello(): return {'pesan': 'Halo'}  
-if __name__ == '__main__': app.run(host='0.0.0.0', port=5000) 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return {'pesan': 'Halo', 'nim': '230705039'}
+
+@app.route('/health')
+def health():
+    return {'status': 'ok'}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
